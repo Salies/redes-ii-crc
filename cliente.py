a166e1e574e70ba875e6e178f585cafd674f766d
Salies <serezane@protonmail.com>
2022-11-17 21:38:46 -0300

    exemplo funcional, mas sem falha forçada

--- cliente.py
+++ cliente.py
@@ -1,7 +1,8 @@
 import socket
 from crc import crc
 
-#s = socket.socket().connect(('127.0.0.1', 6661))
+s = socket.socket()
+s.connect(('127.0.0.1', 6661))
 
 # mensagem a ser enviada: 3 bytes = 24 bits
 # para o gerador serão usados 8 bits, totalizando 32 bits
@@ -13,7 +14,6 @@ gen = 0b11010101 # CRC-8 (0xD5) -- fonte: https://en.wikipedia.org/wiki/Cyclic_r
 # Fonte: https://stackoverflow.com/questions/18815820/how-to-convert-string-to-binary
 msg_b = int(''.join(format(ord(c), 'b').zfill(8) for c in msg), 2)
 
-#print(bin(msg_b))
+s.sendto(crc(msg_b, gen)[0].to_bytes(4, "big", signed = False), ('127.0.0.1', 6661))
 
-res = crc(msg_b, gen)[1]
-print(bin(res))
+s.close()
---
+++ servidor.py
@@ -0,0 +1,32 @@
+import socket
+from crc import crc
+
+gen = 0b11010101
+s = socket.socket()
+s.bind(('', 6661))
+s.listen(1)
+
+while True:
+    conn, addr = s.accept()
+    msg = conn.recv(4)
+    msg = int.from_bytes(msg, "big") # voltando pra inteiro
+    re = crc(msg, gen)[1] # resto
+    # Resto é 0 -> não há erro -> pode decodificar (tratar a mensagem)
+    if(re == 0):
+        # Retira os len(gen) - 1 bits menos significativos do número
+        # no caso, coloco 7.
+        # Depois disso, converto para uma string de binários.
+        # msg_t = mensagem tratada
+        msg_t = '{0:b}'.format((msg >> 7) & (0x7FFFFF)).zfill(24)
+        # Agora, quebro a mensagem para pegar os três caracteres diferentes
+        # Depois, converto o binário dos caracteres para char, junto-os de volta, e exibo
+        # mensagem.
+        # msg_s = msg split
+        msg_s = ''.join([chr(int(msg_t[i:i+8], 2)) for i in range(0, len(msg_t), 8)])
+        print('Mensagem recebida com sucesso:', msg_s)
+    else:
+        print("Falha no recebido da mensagem. Resto: ", str(re) + ".")
+    # mata o servidor quando receber a mensagem
+    if(msg):
+        conn.close()
+        break
