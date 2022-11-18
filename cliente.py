f757f557d7faff5defadc516619aac1c1e7080c8
Salies <serezane@protonmail.com>
2022-11-17 21:46:28 -0300

    indução a erro

--- cliente.py
+++ cliente.py
@@ -14,6 +14,13 @@ gen = 0b11010101 # CRC-8 (0xD5) -- fonte: https://en.wikipedia.org/wiki/Cyclic_r
 # Fonte: https://stackoverflow.com/questions/18815820/how-to-convert-string-to-binary
 msg_b = int(''.join(format(ord(c), 'b').zfill(8) for c in msg), 2)
 
-s.sendto(crc(msg_b, gen)[0].to_bytes(4, "big", signed = False), ('127.0.0.1', 6661))
+msg_crc = crc(msg_b, gen)[0]
+
+#s.sendto(crc(msg_b, gen)[0].to_bytes(4, "big", signed = False), ('127.0.0.1', 6661))
+
+# Forçando erro: flipando alguns bits
+msg_crc ^= 0x25A2A6E5
+
+s.sendto(msg_crc.to_bytes(4, "big", signed = False), ('127.0.0.1', 6661))
 
 s.close()
